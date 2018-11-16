d3d77b220b6577b859f7b10b7e4ccd04e4025a88
Martin <martin@digip.com.ar>
2018-11-16 15:52:00 -0300

    probando imagenes 2

---
+++ AppergerWeb/Controllers/ImagenAzureController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppergerWeb.Controllers
+{
+    public class ImagenAzureController : Controller
+    {
+      
+
+        // GET: ImagenAzure
+        public ActionResult Cargarlistar()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cargarlistar2(string nombre)
+        {
+            var file = Request.Files[0];
+            if (file != null && file.ContentLength>0)
+            {
+                CloudStorageAccount StorageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["appergerstorage_AzureStorageConnectionString"].ConnectionString);
+                CloudBlobClient blobclient = StorageAccount.CreateCloudBlobClient();
+                CloudBlobContainer container = blobclient.GetContainerReference("imagenes2");
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(file.FileName);
+
+                blockBlob.UploadFromStream(file.InputStream);
+
+         
+
+
+
+            }
+            return View();
+
+        }
+
+
+    }
+}
---
+++ AppergerWeb/Models/imagenes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppergerWeb.Models
+{
+    public class imagenes
+    {
+        public imagenes(){}
+    public imagenes(string nombre, string url){
+            this.Nombre = nombre;
+            this.Url = url;
+        }
+
+        public string Nombre { get; set; }
+        public string Url { get; set; }
+    }
+}
