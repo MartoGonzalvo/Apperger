c658334a40837e53ba299a18480b9efa5e679345
MartoGonzalvo <gonzalvomartin@gmail.com>
2018-11-16 23:22:59 -0300

    listando imagenes y vista en navegador

--- AppergerWeb/Controllers/ImagenAzureController.cs
+++ AppergerWeb/Controllers/ImagenAzureController.cs
@@ -6,19 +6,53 @@ using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AppergerWeb.Models;
+
 
 namespace AppergerWeb.Controllers
 {
     public class ImagenAzureController : Controller
     {
-      
+        List<Models.imagenes> imglist = new List<Models.imagenes>();
 
         // GET: ImagenAzure
-        public ActionResult Cargarlistar()
+        public ActionResult Cargarlistar(string nombre)
         {
-           
-            return View();
+            CloudStorageAccount StorageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["appergerstorage_AzureStorageConnectionString"].ConnectionString);
+            CloudBlobClient blobclient = StorageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobclient.GetContainerReference("apperger");
+            //CloudBlockBlob blockBlob = container.GetBlockBlobReference(File.FileName);
+            if (nombre != null) {
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(nombre);
+                blockBlob.Delete();
+                foreach (IListBlobItem item in container.ListBlobs(null, true))
+                {
+                    if (item.GetType() == typeof(CloudBlockBlob))
+                    {
+                        CloudBlockBlob blob = (CloudBlockBlob)item;
+                        imagenes img = new imagenes(blob.Name, blob.Uri.AbsoluteUri);
+                        this.imglist.Add(img);
+                    }
+
+                }
+                return View(imglist);
+            }
+            else
+                foreach (IListBlobItem item in container.ListBlobs(null, true))
+                {
+                    if (item.GetType() == typeof(CloudBlockBlob))
+                    {
+                        CloudBlockBlob blob = (CloudBlockBlob)item;
+                        imagenes img = new imagenes(blob.Name, blob.Uri.AbsoluteUri);
+                        this.imglist.Add(img);
+                    }
+
+                }
+            return View(imglist);
         }
+
+           
+        
         [HttpPost]
         public ActionResult Cargarlistar2(string nombre)
         {
@@ -27,8 +61,9 @@ namespace AppergerWeb.Controllers
             {
                 CloudStorageAccount StorageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["appergerstorage_AzureStorageConnectionString"].ConnectionString);
                 CloudBlobClient blobclient = StorageAccount.CreateCloudBlobClient();
-                CloudBlobContainer container = blobclient.GetContainerReference("imagenes");
+                CloudBlobContainer container = blobclient.GetContainerReference("apperger");
                 CloudBlockBlob blockBlob = container.GetBlockBlobReference(file.FileName);
+                blockBlob.Properties.ContentType="image/jpeg";
 
                 blockBlob.UploadFromStream(file.InputStream);
 
@@ -37,7 +72,7 @@ namespace AppergerWeb.Controllers
 
 
             }
-            return View();
+            return RedirectToAction("Cargarlistar");
 
         }
 
