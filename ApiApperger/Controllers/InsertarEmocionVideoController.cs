c501ab58cd1f709232d090a1865db7da46bc346e
rodrigo reynoso <mcd77.1990@gmail.com>
2018-12-09 15:20:23 -0300

    metodo actualizar emocion elegida en video activities
    metodo actualizar emocion elegida en video activities

---
+++ ApiApperger/Controllers/InsertarEmocionVideoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ApiApperger.Models;
+
+namespace ApiApperger.Controllers
+{
+    public class InsertarEmocionVideoController : ApiController
+    {
+        appergerEntities1 DB = new appergerEntities1();
+
+        [HttpGet]
+        [ActionName("XAMARIN_InsertarEmocionVideo")]
+        // GET: api/Login/5  
+        public HttpResponseMessage Xamarin_InsertarEmocionVideo(int idEmocion, int idVideo)
+        {
+
+
+            var query = DB.VideoTratamientoes.Where(x => x.nIdVideo == idVideo).FirstOrDefault();
+
+            if (query != null)
+            {
+                query.nIdEmocionElegida = idEmocion;
+                DB.SaveChanges();
+
+                return Request.CreateResponse(HttpStatusCode.Unauthorized, "Datos actualizados");
+            }
+            else
+            {
+
+                return Request.CreateResponse(HttpStatusCode.Accepted, "Error al actualizar");
+            }
+        }
+    }
+}
